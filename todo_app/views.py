35fe1470ab274719a17f769390eea3c0ef941819
Abhijith K <abhijith.k61@gmail.com>
2021-08-02 14:37:55 +0530

    list

---
+++ todo_app/views.py
@@ -0,0 +1,40 @@
+#from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from . models import Task
+from .forms import Todoforms
+from django.views.generic import ListView
+
+# Create your views here.
+class TaskListView(ListView):
+    model = Task
+    template_name = "task_view.html"
+    context_object_name = "object1"
+
+
+
+def task_view(request):
+    objct1=Task.objects.all()
+    if request.method=="POST":
+        name=request.POST.get('name')
+        priority=request.POST.get('priority')
+        date=request.POST.get('date')
+
+        obj=Task(name=name,priority=priority,date=date)
+        obj.save()
+    return render(request,'task_view.html',{'object1':objct1})
+
+def delete (request, taskid):
+    task=Task.objects.get(id=taskid)
+    if request.method=='POST':
+        task.delete()
+        return redirect('/')
+    return render(request,'delete.html',{'task':task})
+
+def update(request,id):
+    task=Task.objects.get(id=id)
+    form=Todoforms(request.POST or None, instance=task)
+    if form.is_valid ():
+        form.save()
+        return redirect('/')
+    return render(request,'edit.html',{'task':task,'form':form})
+
